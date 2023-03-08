11b9a32f9e760ac7b8f0b589b7ed52792986401f
joongkyu-park <jk.park.tech@gmail.com>
2023-03-08 16:39:16 +0900

    Chore: 프로젝트 생성 및 초기 프로젝트 설정
    - 프로젝트 생성
    - git init
    - Deployment Target 11.0으로 변경 및 관련 설정
    - Code-based 작업을 위한 설정
    - 세로모드(portrait) 고정
    - 다크모드 금지 설정
    - README 생성
    - .gitignore 생성

---
+++ README.md
@@ -0,0 +1,4 @@
+# 패스트레인(여신티켓) iOS 사전 과제_박중규
+
+## Deployment Target
+- iOS 11.0
---
+++ fastlane_JoongkyuPark/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  fastlane_JoongkyuPark
+//
+//  Created by Apple on 2023/03/08.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
